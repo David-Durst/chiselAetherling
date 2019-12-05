22390c9175515a090cafe6063e38d7cbad9c2d6e
David Durst <davidbdurst@gmail.com>
2019-12-04 23:51:45 -0800

    map2s printing now good, passing tests

---
+++ src/test/scala/aetherling/modules/helpers/NestedPeekPokeTester.scala
@@ -0,0 +1,114 @@
+package aetherling.modules.helpers
+
+import aetherling.types.TupleBundle
+import chisel3.iotesters.PeekPokeTester
+import chisel3.{Aggregate, Bool, Data, MultiIOModule, UInt}
+
+abstract class NestedPeekPokeTester[+T <: MultiIOModule](val c: T ) extends PeekPokeTester(c) {
+  def poke_nested(signal: TupleBundle, values: IndexedSeq[_]): Unit = {
+    values(0) match {
+      case v: Array[_] =>
+        signal.t0b match {
+          case e: TupleBundle => poke_nested(e, v)
+          case e: Aggregate => poke_nested(e, v)
+          case e => throw new Exception(s"Not a Vec or TupleBundle type trying to poke: $e")
+        }
+      case v: Int => poke_nested(signal.t0b, BigInt(v))
+      case v: BigInt => poke_nested(signal.t0b, v)
+      case _ => throw new Exception(s"Cannot poke value ${signal.t0b.getClass.getName}")
+    }
+    values(1) match {
+      case v: Array[_] =>
+        signal.t1b match {
+          case e: TupleBundle => poke_nested(e, v)
+          case e: Aggregate => poke_nested(e, v)
+          case e => throw new Exception(s"Not a Vec or TupleBundle type trying to poke: $e")
+        }
+      case v: Int => poke_nested(signal.t1b, BigInt(v))
+      case v: BigInt => poke_nested(signal.t1b, v)
+      case _ => throw new Exception(s"Cannot poke value ${signal.t1b.getClass.getName}")
+    }
+  }
+
+  def poke_nested(signal: Aggregate, values: IndexedSeq[_]): Unit = {
+    (signal.getElements zip values).foreach{ case (elem, value) =>
+      value match {
+        case v: Array[_] =>
+          elem match {
+            case e: TupleBundle => poke_nested(e, v)
+            case e: Aggregate => poke_nested(e, v)
+            case e => throw new Exception(s"Not a Vec or TupleBundle type trying to poke: $e")
+          }
+        case v: Int => poke_nested(elem, v)
+        case v: BigInt => poke_nested(elem, v)
+        case _ => throw new Exception(s"Cannot poke value ${elem.getClass.getName}")
+      }
+    }
+  }
+
+  def poke_nested(signal: Data, value: Int): Unit = {
+    poke(signal.asUInt(), BigInt(value))
+  }
+  def poke_nested(signal: Data, value: BigInt): Unit = {
+    poke(signal.asUInt(), value)
+  }
+
+
+  def expect_nested(signal: TupleBundle, values: IndexedSeq[_]): Unit = {
+    values(0) match {
+      case v: Array[_] =>
+        signal.t0b match {
+          case e: TupleBundle => expect_nested(e, v)
+          case e: Aggregate => expect_nested(e, v)
+          case e => throw new Exception(s"Not a Vec or TupleBundle type trying to expect: $e")
+        }
+      case v: Int => expect_nested(signal.t0b, BigInt(v))
+      case v: BigInt => expect_nested(signal.t0b, v)
+      case _ => throw new Exception(s"Cannot expect value ${signal.t0b.getClass.getName}")
+    }
+    values(1) match {
+      case v: Array[_] =>
+        signal.t1b match {
+          case e: TupleBundle => expect_nested(e, v)
+          case e: Aggregate => expect_nested(e, v)
+          case e => throw new Exception(s"Not a Vec or TupleBundle type trying to expect: $e")
+        }
+      case v: Int => expect_nested(signal.t1b, BigInt(v))
+      case v: BigInt => expect_nested(signal.t1b, v)
+      case _ => throw new Exception(s"Cannot expect value ${signal.t1b.getClass.getName}")
+    }
+  }
+
+  def expect_nested(signal: Aggregate, values: IndexedSeq[_]): Unit = {
+    (signal.getElements zip values).foreach{ case (elem, value) =>
+      value match {
+        case v: Array[_] =>
+          elem match {
+            case e: TupleBundle => expect_nested(e, v)
+            case e: Aggregate => expect_nested(e, v)
+            case e => throw new Exception(s"Not a Vec or TupleBundle type trying to expect: $e")
+          }
+        case v: Int => expect_nested(elem, v)
+        case v: BigInt => expect_nested(elem, v)
+        case _ => throw new Exception(s"Cannot expect value ${elem.getClass.getName}")
+      }
+    }
+  }
+
+  def expect_nested(signal: Data, value: Int): Unit = {
+    expect(signal.asUInt(), BigInt(value))
+  }
+  def expect_nested(signal: Data, value: BigInt): Unit = {
+    expect(signal.asUInt(), value)
+  }
+
+  def peek_str(signal: Data): String = {
+    signal match {
+      case s: TupleBundle => s"Tuple(${peek_str(s.t0b)}, ${peek_str(s.t1b)})"
+      case s: Aggregate => s"Vec(${s.getElements.map(peek_str).reduce((l,r) => l + ", " + r)})"
+      case s: UInt => peek(s).toString
+      case s: Bool => peek(s).toString
+      case s => s"Cannot peek_str $s which has class ${s.getClass}"
+    }
+  }
+}
--- src/test/scala/aetherling/modules/higherorder/Map2SUnitTests.scala
+++ src/test/scala/aetherling/modules/higherorder/Map2SUnitTests.scala
@@ -1,118 +1,19 @@
 package aetherling.modules.higherorder
 
+import aetherling.modules.helpers.NestedPeekPokeTester
 import aetherling.modules.{Abs, AtomTuple}
 import aetherling.types.{STInt, TupleBundle}
 import chisel3._
 import chisel3.iotesters.{ChiselFlatSpec, PeekPokeTester, Pokeable}
 
-abstract class NestedPeekPokeTester[+T <: MultiIOModule](val c: T ) extends PeekPokeTester(c) {
-  def poke_nested(signal: TupleBundle, values: IndexedSeq[_]): Unit = {
-    values(0) match {
-      case v: Array[_] =>
-        signal.t0b match {
-          case e: TupleBundle => poke_nested(e, v)
-          case e: Aggregate => poke_nested(e, v)
-          case e => throw new Exception(s"Not a Vec or TupleBundle type trying to poke: $e")
-        }
-      case v: Int => poke_nested(signal.t0b, BigInt(v))
-      case v: BigInt => poke_nested(signal.t0b, v)
-      case _ => throw new Exception(s"Cannot poke value ${signal.t0b.getClass.getName}")
-    }
-    values(1) match {
-      case v: Array[_] =>
-        signal.t1b match {
-          case e: TupleBundle => poke_nested(e, v)
-          case e: Aggregate => poke_nested(e, v)
-          case e => throw new Exception(s"Not a Vec or TupleBundle type trying to poke: $e")
-        }
-      case v: Int => poke_nested(signal.t1b, BigInt(v))
-      case v: BigInt => poke_nested(signal.t1b, v)
-      case _ => throw new Exception(s"Cannot poke value ${signal.t1b.getClass.getName}")
-    }
-  }
-
-  def poke_nested(signal: Aggregate, values: IndexedSeq[_]): Unit = {
-    (signal.getElements zip values).foreach{ case (elem, value) =>
-      value match {
-        case v: Array[_] =>
-          elem match {
-            case e: TupleBundle => poke_nested(e, v)
-            case e: Aggregate => poke_nested(e, v)
-            case e => throw new Exception(s"Not a Vec or TupleBundle type trying to poke: $e")
-          }
-        case v: Int => poke_nested(elem, v)
-        case v: BigInt => poke_nested(elem, v)
-        case _ => throw new Exception(s"Cannot poke value ${elem.getClass.getName}")
-      }
-    }
-  }
-
-  def poke_nested(signal: Data, value: Int): Unit = {
-    poke(signal.asUInt(), BigInt(value))
-  }
-  def poke_nested(signal: Data, value: BigInt): Unit = {
-    poke(signal.asUInt(), value)
-  }
-
-
-  def expect_nested(signal: TupleBundle, values: IndexedSeq[_]): Unit = {
-    values(0) match {
-      case v: Array[_] =>
-        signal.t0b match {
-          case e: TupleBundle => expect_nested(e, v)
-          case e: Aggregate => expect_nested(e, v)
-          case e => throw new Exception(s"Not a Vec or TupleBundle type trying to expect: $e")
-        }
-      case v: Int => expect_nested(signal.t0b, BigInt(v))
-      case v: BigInt => expect_nested(signal.t0b, v)
-      case _ => throw new Exception(s"Cannot expect value ${signal.t0b.getClass.getName}")
-    }
-    values(1) match {
-      case v: Array[_] =>
-        signal.t1b match {
-          case e: TupleBundle => expect_nested(e, v)
-          case e: Aggregate => expect_nested(e, v)
-          case e => throw new Exception(s"Not a Vec or TupleBundle type trying to expect: $e")
-        }
-      case v: Int => expect_nested(signal.t1b, BigInt(v))
-      case v: BigInt => expect_nested(signal.t1b, v)
-      case _ => throw new Exception(s"Cannot expect value ${signal.t1b.getClass.getName}")
-    }
-  }
-
-  def expect_nested(signal: Aggregate, values: IndexedSeq[_]): Unit = {
-    (signal.getElements zip values).foreach{ case (elem, value) =>
-      value match {
-        case v: Array[_] =>
-          elem match {
-            case e: TupleBundle => expect_nested(e, v)
-            case e: Aggregate => expect_nested(e, v)
-            case e => throw new Exception(s"Not a Vec or TupleBundle type trying to expect: $e")
-          }
-        case v: Int => expect_nested(elem, v)
-        case v: BigInt => expect_nested(elem, v)
-        case _ => throw new Exception(s"Cannot expect value ${elem.getClass.getName}")
-      }
-    }
-  }
-
-  def expect_nested(signal: Data, value: Int): Unit = {
-    expect(signal.asUInt(), BigInt(value))
-  }
-  def expect_nested(signal: Data, value: BigInt): Unit = {
-    expect(signal.asUInt(), value)
-  }
-}
-
 class Map2STupleUnitTester(c: Map2S) extends NestedPeekPokeTester(c) {
-  for(i <- -10 to 10 by 1) {
-    for(j <- 0 to 3 by 1) {
-      poke_nested(c.in0, (0 to 3).map(j => BigInt(i*j)))
-      poke_nested(c.in1, (0 to 3).map(j => BigInt(i*j+17)))
-      //println(s"in: ${peek(c.io.in)}")
-      //println(s"out: ${peek(c.io.out)}")
-      expect_nested(c.out, (0 to 3).map(j => Array(i*j, i*j+17)))
-    }
+  for(i <- 0 to 10 by 1) {
+    poke_nested(c.in0, (0 to 3).map(j => BigInt(i*j)))
+    poke_nested(c.in1, (0 to 3).map(j => BigInt(i*j+17)))
+    println(s"in0: ${peek_str(c.in0)}")
+    println(s"in1: ${peek_str(c.in1)}")
+    println(s"out: ${peek_str(c.out)}")
+    expect_nested(c.out, (0 to 3).map(j => Array(i*j, i*j+17)))
   }
 }
 
