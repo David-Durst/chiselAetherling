95dee279e00d00ce34f49efc803d4353ed980494
David Durst <davidbdurst@gmail.com>
2020-01-10 16:49:44 -0800

    added const and intialdelay counter

---
+++ src/main/scala/aetherling/modules/Const.scala
@@ -0,0 +1,21 @@
+package aetherling.modules
+
+import aetherling.modules.helpers.{NullaryInterface, ValidInterface, InitialDelayCounter}
+import aetherling.types._
+import chisel3.MultiIOModule
+import chisel3._
+import chisel3.util.Counter
+
+class Const[T](t: STTypeDefinition, data: Seq[Data], delay: Int)
+  extends MultiIOModule with NullaryInterface with ValidInterface {
+  override val out = IO(Output(t.chiselRepr()))
+
+  val enabled = if (delay == 0) true.B else {
+    val delay_counter = Module(new InitialDelayCounter(delay))
+    delay_counter.valid_up := valid_up
+    delay_counter.valid_down
+  }
+
+  val rom = VecInit(data)
+  val r = rom(Counter(rom.length).value)
+}
---
+++ src/main/scala/aetherling/modules/helpers/InitialDelayCounter.scala
@@ -0,0 +1,23 @@
+package aetherling.modules.helpers
+
+import aetherling.modules.helpers.{UnaryInterface, ValidInterface}
+import chisel3.util.log2Ceil
+import chisel3._
+
+class InitialDelayCounter(val n: Int) extends MultiIOModule with ValidInterface {
+  require(n >= 0)
+  val value = if (n > 1) RegInit(0.U(log2Ceil(n+1).W)) else 0.U
+
+
+  /** Increment the counter, returning whether the counter currently is at the
+    * maximum and will wrap. The incremented value is registered and will be
+    * visible on the next cycle.
+    */
+  if (n > 1) {
+    valid_down := value === n.U
+    when (value < n.U && valid_up) { value := value + 1.U }
+  } else {
+    valid_down := valid_up
+  }
+}
+
