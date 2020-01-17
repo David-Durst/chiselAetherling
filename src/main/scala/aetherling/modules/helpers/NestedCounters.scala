c93473b4fb9bb8e8ddf195cd9629aba63a786a68
David Durst <davidbdurst@gmail.com>
2020-01-16 17:04:49 -0800

    tested nested counters

--- src/main/scala/aetherling/modules/helpers/NestedCounters.scala
+++ src/main/scala/aetherling/modules/helpers/NestedCounters.scala
@@ -7,31 +7,33 @@ import chisel3._
 import chisel3.util.Counter
 
 /**
-  * Create a counter that emits value on each valid element of a nested type
+  * Create a counter that emits value on each valid element of a nested type and tracks the index of the valid element
   * @param t - the type
-  * @param has_cur_valid - whether a port should emit an integer of the current valid element
   * @param valid_down_when_ce_disabled - should this emit valid when on a valid element but CE is enabled
   */
-class NestedCounters(val t: STTypeDefinition, has_cur_valid: Boolean,
+class NestedCountersWithNumValid(val t: STTypeDefinition, has_cur_valid: Boolean,
                       valid_down_when_ce_disabled: Boolean) extends MultiIOModule {
   val CE = IO(Input(Bool()))
   val valid = IO(Output(Bool()))
   // is this the last element of the type
   val last = IO(Output(Bool()))
 
-  val inner_nested_counter = Module(new _NestedCounters(t, valid_down_when_ce_disabled))
+  private val inner_nested_counter = Module(new NestedCounters(t, valid_down_when_ce_disabled))
   inner_nested_counter.CE := CE
   valid := inner_nested_counter.valid
   last := inner_nested_counter.last
 
-  if (has_cur_valid) {
-    val (cur_valid_value, _) = Counter(CE && inner_nested_counter.valid, t.validClocks())
-    val cur_valid = IO(Output(UInt(cur_valid_value.getWidth.W)))
-    cur_valid := cur_valid_value
-  }
+  private val (cur_valid_value, _) = Counter(CE && inner_nested_counter.valid, t.validClocks())
+  val cur_valid = IO(Output(UInt(cur_valid_value.getWidth.W)))
+  cur_valid := cur_valid_value
 }
 
-class _NestedCounters(t: STTypeDefinition, valid_down_when_ce_disabled: Boolean) extends MultiIOModule {
+/**
+  * Create a counter that emits value on each valid element of a nested type
+  * @param t - the type
+  * @param valid_down_when_ce_disabled - should this emit valid when on a valid element but CE is enabled
+  */
+class NestedCounters(val t: STTypeDefinition, valid_down_when_ce_disabled: Boolean) extends MultiIOModule {
   val CE = IO(Input(Bool()))
   val valid = IO(Output(Bool()))
   val last = IO(Output(Bool()))
@@ -39,19 +41,21 @@ class _NestedCounters(t: STTypeDefinition, valid_down_when_ce_disabled: Boolean)
 
   if (t.isInstanceOf[TSeq[_]]) {
     val t_tseq = t.asInstanceOf[TSeq[STTypeDefinition]]
-    val inner_counter = Module(new _NestedCounters(t_tseq.t, valid_down_when_ce_disabled))
+    val inner_counter = Module(new NestedCounters(t_tseq.t, valid_down_when_ce_disabled))
     // wrap is when 0 after being n-1, last is when currently n-1
     val (outer_counter_value, _) = Counter(CE && inner_counter.last, t_tseq.n + t_tseq.i)
 
     last := (outer_counter_value === (t_tseq.n + t_tseq.i - 1).U) && inner_counter.last
     valid := (outer_counter_value < t_tseq.n.U) && inner_counter.valid
+    inner_counter.CE := CE
   }
   else if (t.isInstanceOf[NestedSTType[_]]) {
     val t_nested = t.asInstanceOf[NestedSTType[STTypeDefinition]]
-    val inner_counter = Module(new _NestedCounters(t_nested.t, valid_down_when_ce_disabled))
+    val inner_counter = Module(new NestedCounters(t_nested.t, valid_down_when_ce_disabled))
 
     last := inner_counter.last
     valid := inner_counter.valid
+    inner_counter.CE := CE
   }
   else {
     last := true.B
@@ -62,7 +66,5 @@ class _NestedCounters(t: STTypeDefinition, valid_down_when_ce_disabled: Boolean)
       valid := CE
     }
   }
-
-
 }
 
--- src/test/scala/aetherling/modules/helpers/NestedCounters.scala
+++ src/test/scala/aetherling/modules/helpers/NestedCounters.scala
@@ -1,33 +1,72 @@
 package aetherling.modules.helpers
 
 import aetherling.modules.{Abs, Add, AddUnitTester}
-import aetherling.types.STInt
+import aetherling.types._
+import chisel3._
 import chisel3.iotesters
-import chisel3.iotesters.{ChiselFlatSpec, PeekPokeTester}
+import chisel3.iotesters.{ChiselFlatSpec}
 
-class NestedCounterTester(c: NestedCounters, validClocks: Vector[Boolean], countValids: Boolean) extends NestedPeekPokeTester(c) {
-  var num_valids = 0
+class NestedCounterTester(c: NestedCounters, validClocks: Vector[Boolean]) extends NestedPeekPokeTester(c) {
+  poke(c.CE, true)
   for(i <- 0 to c.t.time() - 1) {
     println(s"clk: ${i}")
     //println(s"in: ${peek(c.io.in)}")
     //println(s"out: ${peek(c.io.out)}")
-    expect(c.valid)
-    expect(c.O, scala.math.abs(i))
+    expect(c.valid, validClocks(i))
     step(1)
-    num_valids += 1
+  }
+}
+
+class NestedCounterWithNumValidTester(c: NestedCountersWithNumValid, validClocks: Vector[Boolean]) extends NestedPeekPokeTester(c) {
+  var numValid = 0
+  poke(c.CE, true)
+  for(i <- 0 to c.t.time() - 1) {
+    println(s"clk: ${i}")
+    //println(s"in: ${peek(c.io.in)}")
+    //println(s"out: ${peek(c.io.out)}")
+    expect(c.valid, validClocks(i))
+    expect(c.cur_valid, numValid)
+    if(validClocks(i) == true) {
+      numValid += 1
+    }
+    step(1)
   }
 }
 
 class NestedCounterTests extends ChiselFlatSpec {
-  "Add" should "add two ints correctly" in {
-    iotesters.Driver.execute(Array("--backend-name", "verilator"), () => new Add(new STInt(8))) {
-      c => new AddUnitTester(c)
+  "NestedCounters" should "emit valid always for atoms" in {
+    iotesters.Driver.execute(Array("--backend-name", "verilator"), () => new NestedCounters(STInt(8), true)) {
+      c => new NestedCounterTester(c, Vector(true))
     } should be(true)
   }
 
-  "Abs" should "take absolute value of an int correctly" in {
-    iotesters.Driver.execute(Array("--backend-name", "verilator"), () => new Abs(STInt(8))) {
-      c => new AbsUnitTester(c)
+  "NestedCounters" should "emit valid always for SSeq" in {
+    iotesters.Driver.execute(Array("--backend-name", "verilator"), () => new NestedCounters(SSeq(3, STInt(8)), true)) {
+      c => new NestedCounterTester(c, Vector(true))
+    } should be(true)
+  }
+
+  "NestedCounters" should "emit valid always for TSeq where always valid" in {
+    iotesters.Driver.execute(Array("--backend-name", "verilator"), () => new NestedCounters(TSeq(3, 0, STInt(8)), true)) {
+      c => new NestedCounterTester(c, Vector(true, true, true))
+    } should be(true)
+  }
+
+  "NestedCounters" should "emit valid and invalid for TSeq where sometimes valid" in {
+    iotesters.Driver.execute(Array("--backend-name", "verilator"), () => new NestedCounters(TSeq(3, 2, STInt(8)), true)) {
+      c => new NestedCounterTester(c, Vector(true, true, true, false, false))
+    } should be(true)
+  }
+
+  "NestedCounters" should "emit valid and invalid for nested TSeq where sometimes valid" in {
+    iotesters.Driver.execute(Array("--backend-name", "verilator"), () => new NestedCounters(TSeq(1, 1, TSeq(2, 1, STInt(8))), true)) {
+      c => new NestedCounterTester(c, Vector(true, true, false, false, false, false))
+    } should be(true)
+  }
+
+  "NestedCountersWithNumValid" should "emit correct counts for nested TSeq where sometimes valid" in {
+    iotesters.Driver.execute(Array("--backend-name", "verilator"), () => new NestedCounters(TSeq(2, 1, TSeq(1, 1, STInt(8))), true)) {
+      c => new NestedCounterTester(c, Vector(true, false, true, false, false, false))
     } should be(true)
   }
 }
