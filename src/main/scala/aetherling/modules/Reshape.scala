3c7deb739ba0c3d232d3cfb00d9dcb2685e94cf1
David Durst <davidbdurst@gmail.com>
2020-01-18 15:21:00 -0800

    reshape crashes on construction rather than having undefined port

--- src/main/scala/aetherling/modules/Reshape.scala
+++ src/main/scala/aetherling/modules/Reshape.scala
@@ -8,6 +8,7 @@ import chisel3.util.Counter
 
 class Reshape(t_in: STTypeDefinition, t_out: STTypeDefinition ) extends MultiIOModule
   with UnaryInterface with ValidInterface {
-  val I = ???
-  val O = ???
+  val I = IO(Input(t_in.chiselRepr()))
+  val O = IO(Output(t_out.chiselRepr()))
+  throw new NotImplementedError("Reshape Not Implemented")
 }
