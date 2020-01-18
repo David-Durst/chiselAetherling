6b0c87289550060a325ebf71a69a35e119fc675d
David Durst <davidbdurst@gmail.com>
2020-01-17 16:54:41 -0800

    added partitions and unparititons

---
+++ src/main/scala/aetherling/modules/Partition.scala
@@ -0,0 +1,32 @@
+package aetherling.modules
+
+import aetherling.modules.helpers._
+import aetherling.modules.higherorder._
+import aetherling.types._
+import chisel3._
+
+class PartitionS(no: Int, ni: Int, elem_t: STTypeDefinition) extends MultiIOModule
+  with UnaryInterface with ValidInterface {
+  val I = IO(Input(SSeq(no * ni, elem_t).chiselRepr()))
+  val O = IO(Output(SSeq(no, SSeq(ni, elem_t)).chiselRepr()))
+
+  for (i <- 0 to no - 1) {
+    for (j <- 0 to ni - 1) {
+      O(i).asInstanceOf[Vec[Data]](j) := I(i*ni + j)
+    }
+  }
+  valid_down := valid_up
+}
+
+class UnpartitionS(no: Int, ni: Int, elem_t: STTypeDefinition) extends MultiIOModule
+  with UnaryInterface with ValidInterface {
+  val I = IO(Input(SSeq(no, SSeq(ni, elem_t)).chiselRepr()))
+  val O = IO(Output(SSeq(no * ni, elem_t).chiselRepr()))
+
+  for (i <- 0 to no - 1) {
+    for (j <- 0 to ni - 1) {
+      I(i*ni + j) := O(i).asInstanceOf[Vec[Data]](j)
+    }
+  }
+  valid_down := valid_up
+}
