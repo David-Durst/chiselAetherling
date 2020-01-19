6bd9bece2adb3ffc342279fb2bf8b4874e8c1ce2
David Durst <davidbdurst@gmail.com>
2020-01-18 19:07:34 -0800

    one attempt at snd and first ahndling vectors

--- src/main/scala/aetherling/types/Helpers.scala
+++ src/main/scala/aetherling/types/Helpers.scala
@@ -7,8 +7,40 @@ import chisel3._
   */
 object Helpers {
   def throughput[T <: STTypeDefinition](t: T): Int = t.length() / t.time()
-  def getFstTuple(tupleBundle: Data): Data = tupleBundle.asInstanceOf[TupleBundle].t0b
-  def getSndTuple(tupleBundle: Data): Data = tupleBundle.asInstanceOf[TupleBundle].t1b
+
+  def stripVec1(data: Data): Data = {
+    data match {
+      case d: Vec[Data] if d.length == 1 => stripVec1(d(0))
+      case d => d
+    }
+  }
+
+  /**
+    * Get the first element of a tuple. If the tuple is nested inside
+    * Vecs of length 1, get the first element with the same nesting.
+    */
+  def getFstTuple(data: Data): Data = {
+    if (data.isInstanceOf[Vec[Data]] && data.asInstanceOf[Vec[Data]].length == 1) {
+      // used to create hardware of desired output type
+      val vecWire = Wire(get)
+      data.asInstanceOf[Vec[Data]].map(getFstTuple)
+    }
+    else {
+      data.asInstanceOf[TupleBundle].t0b
+    }
+  }
+  /**
+    * Get the first element of a tuple. If the tuple is nested inside
+    * Vecs of length 1, get the first element with the same nesting.
+    */
+  def getSndTuple(data: Data): Data = {
+    if (data.isInstanceOf[Vec[Data]] && data.asInstanceOf[Vec[Data]].length == 1) {
+      Vec(1, getSndTuple(data.asInstanceOf[Vec[Data]](0)))
+    }
+    else {
+      data.asInstanceOf[TupleBundle].t1b
+    }
+  }
   def getTupleElem(tupleBundle: Data): IndexedSeq[Data] =
     Vector(getFstTuple(tupleBundle), getSndTuple(tupleBundle))
 }
