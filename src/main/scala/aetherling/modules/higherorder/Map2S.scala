ac45ec0c1316316032d6dd41d27d6edc8058708e
David Durst <davidbdurst@gmail.com>
2020-01-24 00:00:26 -0800

    map2s uses clone, reduce doesn't have any clone changes

--- src/main/scala/aetherling/modules/higherorder/Map2S.scala
+++ src/main/scala/aetherling/modules/higherorder/Map2S.scala
@@ -2,21 +2,26 @@ package aetherling.modules.higherorder
 
 import aetherling.modules.helpers.{BinaryInterface, ValidInterface}
 import chisel3._
+import chisel3.experimental.CloneModuleAsRecord
 
 class Map2S(n: Int, t: => MultiIOModule with BinaryInterface with ValidInterface)
   extends MultiIOModule with BinaryInterface with ValidInterface {
-  val ops = (0 to (n-1)).map(_ => Module(t))
-  val fst_op = ops.head
+  val fst_op = Module(t)
+  val other_ops = if (n == 1) Vector() else (0 to (n-2)).map(_ => CloneModuleAsRecord(fst_op))
 
   override val I0 = IO(Input(Vec(n, chiselTypeOf(fst_op.I0))))
   override val I1 = IO(Input(Vec(n, chiselTypeOf(fst_op.I1))))
   override val O = IO(Output(Vec(n, chiselTypeOf(fst_op.O))))
 
-  ops.zipWithIndex.foreach { case (op, i) =>
-    op.valid_up := valid_up
-    op.I0 := I0(i)
-    op.I1 := I1(i)
-    O(i) := op.O
+  fst_op.valid_up := valid_up
+  fst_op.I0 := I0(0)
+  fst_op.I1 := I1(0)
+  O(0) := fst_op.O
+  other_ops.zipWithIndex.foreach { case (op, i) =>
+    op("valid_up").asInstanceOf[Bool] := valid_up
+    op("I0").asInstanceOf[fst_op.I0.type] := I0(i+1)
+    op("I1").asInstanceOf[fst_op.I1.type] := I1(i+1)
+    O(i+1) := op("O").asInstanceOf[fst_op.O.type]
   }
-  valid_down := ops map { op => op.valid_down } reduce { (vdown0, vdown1) => vdown0 & vdown1 }
+  valid_down := other_ops.foldLeft(fst_op.valid_down) { case (vdown, op) => vdown && op("valid_down").asInstanceOf[Bool] }
 }
--- src/main/scala/aetherling/modules/higherorder/ReduceS.scala
+++ src/main/scala/aetherling/modules/higherorder/ReduceS.scala
@@ -4,9 +4,7 @@ import aetherling.modules.helpers._
 import aetherling.modules.higherorder._
 import aetherling.types._
 import chisel3._
-import chisel3.experimental.CloneModuleAsRecord
 import chisel3.util.Counter
-
 import math._
 import scala.collection.mutable
 
@@ -19,39 +17,29 @@ class ReduceS(n: Int, op: => MultiIOModule with UnaryInterface, elem_t: STTypeDe
     O := RegNext(I)
   }
   else {
-    val fst_op = Module(op)
-    val other_ops = if (n == 1) Vector() else (0 to (n-2)).map(_ => CloneModuleAsRecord(fst_op))
-    val unwired_ins = mutable.Set(0 to 2 * n - 1:_*)
-
-    def get_I_from_op_i(i: Int) = {
-      if (i == 0) {
-        fst_op.I
-      }
-      else {
-        other_ops(i-1)("I").asInstanceOf[fst_op.I.type]
-      }
-    }
+    val ops = (0 to (n-2)).map(_ => Module(op))
+    val unwired_ins_array = ops flatMap(op => Helpers.getTupleElem(Helpers.stripVec1(op.I)))
+    val unwired_ins = mutable.Set(unwired_ins_array:_*)
 
     // wire ops in tree
     for (i <- 0 to n - 2) {
       if (i == 0) {
-        O(0) := RegNext(op.O)
+        O(0) := RegNext(ops(0).O)
       }
       else if (i % 2 == 0) {
-        val op_input = Helpers.getFstTuple(Helpers.stripVec1(get_I_from_op_i(i / 2 - 1)))
-        op_input := Helpers.stripVec1(other_ops(i)("O").asInstanceOf[fst_op.O.type])
-        unwired_ins -= i
+        val op_input = Helpers.getFstTuple(Helpers.stripVec1(ops(i / 1 - 1).I))
+        op_input := Helpers.stripVec1(ops(i).O)
+        unwired_ins -= op_input
       }
       else {
-        val op_input = Helpers.getSndTuple(Helpers.stripVec1(get_I_from_op_i(round(ceil(i / 2.0)).toInt - 1)))
-        op_input := Helpers.stripVec1(other_ops(i)("O").asInstanceOf[fst_op.O.type])
-        unwired_ins -= i
+        val op_input = Helpers.getSndTuple(Helpers.stripVec1(ops(round(ceil(i / 2.0)).toInt - 1).I))
+        op_input := Helpers.stripVec1(ops(i).O)
+        unwired_ins -= op_input
       }
     }
 
     // wire inputs to tree
-    for (unwired_in <- unwired_ins) {
-      val 
+    for ((unwired_in, i) <- unwired_ins.view.zipWithIndex) {
       unwired_in := Helpers.stripVec1(I(i))
     }
   }
