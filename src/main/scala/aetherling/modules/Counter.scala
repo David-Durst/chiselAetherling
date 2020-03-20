37c7b5c32af2a81932a4b5d376b514f2b09bac9d
David Durst <davidbdurst@gmail.com>
2020-03-20 13:52:37 -0700

    counter passsing tests for all possible rewrites

--- src/main/scala/aetherling/modules/Counter.scala
+++ src/main/scala/aetherling/modules/Counter.scala
@@ -18,8 +18,16 @@ class Counter_S(val n: Int, val incr_amount: Int, val int_type: STInt, val delay
   override val O = IO(Output(Vec(n, int_type.chiselRepr())))
   val values = for (i <- 0 to n-1) yield Counter_Helpers.int_to_chisel_int(i*incr_amount, int_type)
 
+  val enabled = if (delay == 0) true.B else {
+    val delay_counter = Module(new InitialDelayCounter(delay))
+    // delay handles when to start this counter. don't listen to valid signal.
+    // just handle valid signal to make interfaces uniform for Haskell compiler.
+    delay_counter.valid_up := true.B
+    delay_counter.valid_down
+  }
+
   O := VecInit(values)
-  valid_down := true.B
+  valid_down := enabled
 }
 
 /**
@@ -45,18 +53,18 @@ class Counter_T(val n: Int, val i: Int, val incr_amount: Int, val int_type: STIn
   val counter_value = RegInit(int_type.chiselRepr(), Counter_Helpers.int_to_chisel_int(0, int_type))
 
   if (int_type.signed) {
-    val wrap = counter_value.asInstanceOf[SInt] === (n+i-1).S
+    val wrap = counter_value.asInstanceOf[SInt] === ((n+i-1)*incr_amount).S
     when (enabled && wrap) { counter_value.asInstanceOf[SInt] := 0.S }
     .elsewhen(enabled) { counter_value.asInstanceOf[SInt] := counter_value.asInstanceOf[SInt] +
       incr_amount.S(int_type.width.W)}
   } else {
-    val wrap = counter_value.asInstanceOf[UInt] === (n+i-1).U
+    val wrap = counter_value.asInstanceOf[UInt] === ((n+i-1)*incr_amount).U
     when (enabled && wrap) { counter_value.asInstanceOf[UInt] := 0.U }
     .elsewhen(enabled) {counter_value.asUInt := counter_value.asUInt + incr_amount.U(int_type.width.W)}
   }
 
   O := counter_value
-  valid_down := true.B
+  valid_down := enabled
 }
 
 
@@ -75,14 +83,16 @@ class Counter_TS(val no: Int, val io: Int, val ni: Int, val incr_amount: Int, va
 
   val counter_t = Module(new Counter_T(no, io, incr_amount*ni, int_type, delay))
   val counter_s = Module(new Counter_S(ni, incr_amount, int_type, delay))
+  counter_t.valid_up := valid_up
+  counter_s.valid_up := valid_up
 
   if (int_type.signed) {
-    for (i <- 0 to ni) {
-      O := counter_s.O(i).asInstanceOf[SInt] + counter_t.O.asInstanceOf[SInt]
+    for (i <- 0 to ni-1) {
+      O(i) := counter_s.O(i).asInstanceOf[SInt] + counter_t.O.asInstanceOf[SInt]
     }
   } else {
-    for (i <- 0 to ni) {
-      O := counter_s.O(i).asInstanceOf[UInt] + counter_t.O.asInstanceOf[UInt]
+    for (i <- 0 to ni-1) {
+      O(i) := counter_s.O(i).asInstanceOf[UInt] + counter_t.O.asInstanceOf[UInt]
     }
   }
 
@@ -115,6 +125,9 @@ class Counter_TN(val ns: IndexedSeq[Int], val is: IndexedSeq[Int], val incr_amou
     delay_counter.valid_up := true.B
     delay_counter.valid_down
   }
+  //printf(p"Nested_counters.last: ${nested_counters.last}\n")
+  //printf(p"Nested_counters.valid: ${nested_counters.valid}\n")
+  //printf(p"enabled: ${enabled}\n")
 
   nested_counters.CE := enabled
 
@@ -122,11 +135,11 @@ class Counter_TN(val ns: IndexedSeq[Int], val is: IndexedSeq[Int], val incr_amou
 
   if (int_type.signed) {
     when (enabled && nested_counters.last) { counter_value.asInstanceOf[SInt] := 0.S }
-      .elsewhen(enabled) { counter_value.asInstanceOf[SInt] := counter_value.asInstanceOf[SInt] +
+      .elsewhen(enabled && nested_counters.valid) { counter_value.asInstanceOf[SInt] := counter_value.asInstanceOf[SInt] +
         incr_amount.S(int_type.width.W)}
   } else {
     when (enabled && nested_counters.last) { counter_value.asInstanceOf[UInt] := 0.U }
-      .elsewhen(enabled) {counter_value.asUInt := counter_value.asUInt + incr_amount.U(int_type.width.W)}
+      .elsewhen(enabled && nested_counters.valid) {counter_value.asUInt := counter_value.asUInt + incr_amount.U(int_type.width.W)}
   }
 
 
